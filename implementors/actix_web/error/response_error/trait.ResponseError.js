a0d9edb95095c261169bc6d1af5067cc1ec56cd4
joelwurtz <jwurtz@jolicode.com>
2023-01-27 08:46:14 +0000

    Deploying to gh-pages from @ joelwurtz/actix-web@e3ea0b68d7fbc0e64e108b01cd18cfb4351fe655 🚀

---
+++ implementors/actix_web/error/response_error/trait.ResponseError.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {
+"actix_multipart":[["impl ResponseError for <a class=\"enum\" href=\"actix_multipart/enum.MultipartError.html\" title=\"enum actix_multipart::MultipartError\">MultipartError</a>"]],
+"actix_web":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ implementors/actix_web/extract/trait.FromRequest.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {
+"actix_multipart":[["impl FromRequest for <a class=\"struct\" href=\"actix_multipart/struct.Multipart.html\" title=\"struct actix_multipart::Multipart\">Multipart</a>"]],
+"actix_web":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
